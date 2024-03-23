bedf16cfa86cceacd6dc3d61455a4f3d3bd72f8a
MuktharAmiyan <amiyanmukthar@gmail.com>
2024-03-23 13:58:27 +0530

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by Mukthar Amiyan on 23/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
